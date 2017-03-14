a6d309b88e8dec2bcbbf2faae63041d073692028
Szymon <s.kolodziejczak95@gmail.com>
2017-03-14 09:13:15 +0100

    jsoup

--- app/src/main/java/pt/webscraping/MainActivity.java
+++ app/src/main/java/pt/webscraping/MainActivity.java
@@ -1,34 +1,29 @@
 package pt.webscraping;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends Activity {
+
+   @BindView(R.id.title) TextView title;
+
+   @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        Thread downloadThread = new Thread() {
-            public void run() {
-                Document doc;
-                try {
-                    doc = Jsoup.connect("http://google.pl/").get();
-                    String title = doc.title();
-                    Log.d("asd", title);
-                } catch (IOException e) {
-                    Log.d("asd", "NIE DZIALA");
-                }
-            }
-        };
-        downloadThread.start();
 
+       new getContentAsyncTask("http://www.emag.pl/");
     }
 }
---
+++ app/src/main/java/pt/webscraping/getContentAsyncTask.java
@@ -0,0 +1,55 @@
+package pt.webscraping;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+/**
+ * Created by szymon on 13.03.2017.
+ */
+
+public class getContentAsyncTask extends AsyncTask<Void, Void, Void> {
+
+    private String url;
+    private Document document;
+
+
+    public getContentAsyncTask(String url){
+        this.url = url;
+        this.execute();
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            document = Jsoup.connect(url)
+                .data("", "odkurzacz")
+                .post();
+
+
+            Log.d("dziala?", "?");
+
+
+        } catch (IOException e) {
+            Log.e("doInBackground", e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+
+    }
+}
