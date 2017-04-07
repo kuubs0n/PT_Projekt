d470b62fdea51723be2ccf4cbffa01c8378fd742
Mateusz Kaczmarek <fragles93@gmail.com>
2017-04-07 22:47:11 +0200

    Dodanie w ramach testu pobierania informacji o produkcie z księgarni Matras.pl

--- app/src/main/java/pt/webscraping/GetContentAsyncTask.java
+++ app/src/main/java/pt/webscraping/GetContentAsyncTask.java
@@ -12,6 +12,9 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.sql.Connection;
+import java.util.ArrayList;
+
+import pt.webscraping.entities.Product;
 
 /**
  * Created by szymon on 13.03.2017.
@@ -22,10 +25,11 @@ public class GetContentAsyncTask extends AsyncTask<Void, Void, Void> {
     private String url;
     private Document document;
 
-    public GetContentAsyncTask(String baseUrl, String input) throws UnsupportedEncodingException {
+    private ArrayList<Product> arrayOfProducts = new ArrayList<Product>();
 
+    public GetContentAsyncTask(String baseUrl, String input) throws UnsupportedEncodingException {
         this.url = baseUrl + URLEncoder.encode(input, "UTF-8");
-        this.execute();
+
     }
 
     @Override
@@ -37,21 +41,33 @@ public class GetContentAsyncTask extends AsyncTask<Void, Void, Void> {
     protected Void doInBackground(Void... params) {
         try {
             document = Jsoup.connect(url).get();
-            Elements products = document.select("div.Product-holder-grid");
+            // na sztywno dla księgani matras.pl
+            Elements products = document.select("div.s-item div.s-item-outer");
+
+
 
             for(Element product : products){
-                Log.d("PRODUKT: ", product.select("a[title]").text().toString());
+                Log.d("PRODUKT URL: ", product.select("span.right-side a:first-child").text().toString());
+
+                Product p = new Product();
+                p.setTitle(product.select("span.item-title").text().toString());
+                p.setAuthor(product.select("span.item-author").text().toString());
+                p.setLink(product.select("span.right-side a").first().attr("href"));
+                p.setPrice(product.select("span.item-price").text().toString()); // item-price
+                p.setPhotoURL(product.select("span.image img").attr("data-original").toString()); // span.image img[src]
+
+                this.arrayOfProducts.add(p);
             }
 
         }
         catch (IOException e) {
-            Log.e("doInBackground", e.getMessage());
+            Log.e("GetContentAsyncTask", "doInBackground Unhandled Exception" + e.getMessage());
         }
         return null;
     }
 
     @Override
     protected void onPostExecute(Void result) {
-
+        // TODO: przesłanie arraylist do widoku - Kuba
     }
 }
