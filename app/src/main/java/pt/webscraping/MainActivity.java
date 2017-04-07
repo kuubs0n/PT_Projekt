3ca860d5011a1695ef8c5e21e9ef5a420122a68a
Mateusz Kaczmarek <fragles93@gmail.com>
2017-04-07 22:38:30 +0200

    Dodanie obsługi buttona "search".

--- app/src/main/java/pt/webscraping/MainActivity.java
+++ app/src/main/java/pt/webscraping/MainActivity.java
@@ -2,6 +2,7 @@ package pt.webscraping;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -10,6 +11,8 @@ import android.widget.Toast;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+
+import java.io.UnsupportedEncodingException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,26 +35,40 @@ public class MainActivity extends Activity {
 
         //reklama
         MobileAds.initialize(this, "ca-app-pub-7454303942261775~2781793443");
-        AdRequest adRequest = new AdRequest.Builder().build();
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("93A32EEEBE42D84FCA84328BB291232B") // Mati OnePlusOne
+                .build();
         adView.loadAd(adRequest);
 
         new Database();
     }
 
     @OnClick(R.id.buttonSearch)
-    public void search(View view){
+    public void searchItems(View view) {
 
         String query = editTextQuery.getText().toString();
 
-        if(!query.isEmpty()){
+        if(!query.isEmpty()) {
+            Log.d("MainActivity", "Search box value: + " + query);
+
+            try {
+                String url = "http://www.matras.pl/szukaj/?query=";
+
+                GetContentAsyncTask task = new GetContentAsyncTask(url, query);
+                task.execute();
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                Log.e("MainActivity", "Unhandled Exception in GetContentAsyncTask :<");
+            }
 
         }
     }
 
     @OnClick(R.id.imageButton)
-    public void easterEgg(View view){
+    public void easterEgg(View view) {
         clickCounter++;
-        if(clickCounter > 5){
+        if(clickCounter > 5) {
             imageButton.setImageResource(R.drawable.szymonon);
             Toast.makeText(getApplicationContext(), "Nawiedził Cię Szymon Developer! Wpłać 2 złote i korzystaj dowoli! :)", Toast.LENGTH_LONG).show();
         }
