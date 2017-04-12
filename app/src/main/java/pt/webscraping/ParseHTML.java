43a415599d0760408704f65997e0b14dcccda82e
Mateusz Kaczmarek <fragles93@gmail.com>
2017-04-12 23:41:37 +0200

    Poprawianie wyciągania wartości z drzewa DOM.

--- app/src/main/java/pt/webscraping/ParseHTML.java
+++ app/src/main/java/pt/webscraping/ParseHTML.java
@@ -8,6 +8,7 @@ import java.util.Collection;
 
 import pt.webscraping.entities.ProductView;
 import pt.webscraping.entities.Template;
+import pt.webscraping.entities.URL;
 
 /**
  * Created by Mateusz on 12-Apr-2017.
@@ -22,13 +23,18 @@ public class ParseHTML {
 
         for(Element product : productsEl){
             products.add(new ProductView(
-                    product.select(template.product.title).toString(),
-                    product.select(template.product.author).toString(),
-                    product.select(template.product.link).toString(),
-                    product.select(template.product.price).toString(),
-                    product.select(template.product.photoURL).toString()
+                    product.select(template.product.title).attr("data-name"),
+                    product.select(template.product.author).text(),
+                    //product.select(template.product.link)..attr("href"),
+                    makeBookUrl(template.url, product.select(template.product.link).attr("href")),
+                    product.select(template.product.price).text(),
+                    product.select(template.product.photoURL).attr("src")
             ));
         }
         return products;
+    }
+
+    public static String makeBookUrl(URL url, String link) {
+        return url.getBaseUrl() + link;
     }
 }
