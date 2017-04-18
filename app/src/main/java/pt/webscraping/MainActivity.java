ff497e515a91bf72a55eba2137db74306460af59
Mateusz Kaczmarek <fragles93@gmail.com>
2017-04-18 15:13:14 +0200

    Usunięcie zbędnyuch funkcji, poprawki i komentarze.

--- app/src/main/java/pt/webscraping/MainActivity.java
+++ app/src/main/java/pt/webscraping/MainActivity.java
@@ -1,10 +1,7 @@
 package pt.webscraping;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -29,19 +26,26 @@ import pt.webscraping.entities.Template;
 
 public class MainActivity extends Activity {
 
-    @BindView(R.id.editTextQuery)EditText editTextQuery;
-    @BindView(R.id.adView) AdView adView;
-    @BindView(R.id.imageButton) ImageButton imageButton;
-    @BindView(R.id.textViewAdvFilters) TextView textViewAdvFilters;
-    @BindView(R.id.advFilters) LinearLayout advFilters;
+    @BindView(R.id.editTextQuery)
+    EditText editTextQuery;
 
+    @BindView(R.id.adView)
+    AdView adView;
+
+    @BindView(R.id.imageButton)
+    ImageButton imageButton;
+
+    @BindView(R.id.textViewAdvFilters)
+    TextView textViewAdvFilters;
+
+    @BindView(R.id.advFilters)
+    LinearLayout advFilters;
+
+    // list of bookshops templates
     ArrayList<Template> templates;
-    ProgressDialog progressDialog;
+
 
     private Integer clickCounter = 0;
-
-    private IntentFilter filter = new IntentFilter("pt.webscraping.RESULTS_READY");
-    private BroadcastReceiver broadcast = new GetContentStateReceiver();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,9 +56,8 @@ public class MainActivity extends Activity {
         ButterKnife.bind(this);
 
         // reklama
-        MobileAds.initialize(this, "ca-app-pub-7454303942261775~2781793443");
+        MobileAds.initialize(this, getString(R.string.admob_app_id));
         AdRequest adRequest = new AdRequest.Builder()
-
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .addTestDevice("93A32EEEBE42D84FCA84328BB291232B") // Mati OnePlusOne
                 .build();
@@ -68,38 +71,7 @@ public class MainActivity extends Activity {
         prepareFilters();
     }
 
-    @Override
-    protected void onStart()
-    {
-        Log.d("web.scraper", "MainActivity - onStart.");
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop()
-    {
-        Log.d("web.scraper", "MainActivity - onStop.");
-        super.onStop();
-    }
-
-    @Override
-    public void onResume()
-    {
-        Log.d("web.scraper", "MainActivity - onResume.");
-        super.onResume();
-        registerReceiver(broadcast, filter);
-    }
-
-    @Override
-    public void onPause()
-    {
-        Log.d("web.scraper", "MainActivity - onPause.");
-        unregisterReceiver(broadcast);
-        super.onPause();
-    }
-
-    private void prepareFilters()
-    {
+    private void prepareFilters() {
         for(Template t : templates)
         {
             CheckBox checkBox = new CheckBox(this);
@@ -111,7 +83,7 @@ public class MainActivity extends Activity {
     }
 
     @OnClick(R.id.textViewAdvFilters)
-    public void toggleFilters(View view){
+    public void toggleFilters(View view) {
         if(advFilters.getVisibility() == View.VISIBLE){
             advFilters.setVisibility(View.INVISIBLE);
         }else{
@@ -127,19 +99,18 @@ public class MainActivity extends Activity {
         if(!query.isEmpty())
         {
             // service
-            Intent mServiceIntent = new Intent(this, GetContentIntentService.class);
-            mServiceIntent.putExtra("templates", templates);
-            mServiceIntent.putExtra("searchQuery", query);
+            Intent mServiceIntent = new Intent(this, GetContentIntentService.class)
+                .putExtra("templates", templates)
+                .putExtra("searchQuery", query);
             this.startService(mServiceIntent);
 
+            // redirect to loading screen
             Intent intent = new Intent(this, LoadingActivity.class);
             startActivity(intent);
-
-            // reklamy
         }
         else
         {
-            editTextQuery.setError("Enter a valid book name!");
+            editTextQuery.setError(getString(R.string.form_book_name_entered_invalid));
         }
     }
 
@@ -150,14 +121,5 @@ public class MainActivity extends Activity {
             imageButton.setImageResource(R.drawable.szymonon);
             Toast.makeText(getApplicationContext(), "Nawiedził Cię Szymon Developer! Wpłać 2 złote i korzystaj dowoli! :)", Toast.LENGTH_LONG).show();
         }
-    }
-
-
-    private void prepareProgressDialog(){
-        progressDialog = new ProgressDialog(MainActivity.this,
-                R.style.ProgressBar);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Preparing results");
-        progressDialog.show();
     }
 }
