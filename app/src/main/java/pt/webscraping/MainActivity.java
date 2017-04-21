e6f850c7e6f68f38a55cd285a6f602d30c55caa6
Mateusz Kaczmarek <fragles93@gmail.com>
2017-04-21 19:25:05 +0200

    Usunięcie zbędnych importów.

--- app/src/main/java/pt/webscraping/MainActivity.java
+++ app/src/main/java/pt/webscraping/MainActivity.java
@@ -13,10 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
 
