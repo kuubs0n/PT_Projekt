ea5bdedc6d013a0034b0adc1458b24349a6f061f
Mateusz Kaczmarek <fragles93@gmail.com>
2017-05-24 11:47:15 +0200

    Zamiana putExtra na klase z statycznymi polami.

---
+++ app/src/main/java/pt/webscraping/entities/SearchResult.java
@@ -0,0 +1,13 @@
+package pt.webscraping.entities;
+
+import java.util.ArrayList;
+
+/**
+ * Created by m_kac on 24.05.2017.
+ */
+
+public class SearchResult {
+    public static ArrayList<ProductView> results = new ArrayList<>();
+    public static String searchQuery;
+    public static ArrayList<Template> templates;
+}
